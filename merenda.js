bfdd957f1c4be57533d5b25bf072bf405434b962
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-01 16:11:10 +0200

    added a function

--- merenda.js
+++ merenda.js
@@ -1,3 +1,16 @@
 function sommaInteri(N) {
-    // A voi l'onore!
+    const invalidInput = 'Invalid input';
+    let numb = 0;
+    let tot = 0;
+    if (N < 0 || isNaN(N) || N === '') {
+        return invalidInput
+    }
+    while (tot <= N) {
+        numb += 1;
+        tot += numb;
+        if (tot > N) {
+            numb -= 1
+        }
+    }
+    return numb
 }
